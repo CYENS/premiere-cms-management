2d3f8ba1bb8335222f9ff43a7b46db21bf6a91cb
Chrysostomos Chadjiminas <chadjiminas.chrysostomos@gmail.com>
2025-01-16 21:34:51 +0200

    created PremiereCMSManagementSubsystem

---
+++ Source/PremiereCMSManagement/Private/PremiereCMSManagementSubsystem.cpp
@@ -0,0 +1,45 @@
+
+#include "PremiereCMSManagementSubsystem.h"
+
+#include "GraphQLDataSource.h"
+#include "LogPremiereCMSManagement.h"
+#include "Repositories/SessionRepository.h"
+
+UPremiereCMSManagementSubsystem::UPremiereCMSManagementSubsystem()
+{
+	const FString DefaultGraphQlUrl = TEXT("https://premierecmsdev.medidata.pt/api/graphql");
+	GraphQLUrl = GConfig->GetStringOrDefault(TEXT("CMSSettings"), TEXT("GraphQLUrl"), DefaultGraphQlUrl, GEngineIni);
+	
+	GraphQlDataSource = NewObject<UGraphQLDataSource>();
+	GraphQlDataSource->Initialize(GraphQLUrl);
+
+	SessionRepository = NewObject<USessionRepository>();
+	SessionRepository->Initialize(GraphQlDataSource);
+}
+
+void UPremiereCMSManagementSubsystem::CreateSession(
+	const FString& Title,
+	const FString& OwnerId,
+	const FString& PerformanceId,
+	FOnCreateSessionSuccessDelegate OnCreateSessionSuccess,
+	FOnFailureDelegate OnCreateSessionFailure
+) 
+{
+	FOnGetSessionSuccess OnSuccess;
+	OnSuccess.BindLambda([OnCreateSessionSuccess](const FCMSSession& Session)
+	{
+		OnCreateSessionSuccess.ExecuteIfBound(Session);
+	});
+	FOnFailure OnFailure;
+	OnFailure.BindLambda([OnCreateSessionFailure](const FString& ErrorReason)
+	{
+		OnCreateSessionFailure.ExecuteIfBound(ErrorReason);
+	});
+	SessionRepository->CreateSession(
+		Title,
+		OwnerId,
+		PerformanceId,
+		OnSuccess,
+		OnFailure
+	);
+}
---
+++ Source/PremiereCMSManagement/Public/PremiereCMSManagementSubsystem.h
@@ -0,0 +1,39 @@
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GraphQLDataSource.h"
+#include "Repositories/SessionRepository.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "PremiereCMSManagementSubsystem.generated.h"
+
+DECLARE_DYNAMIC_DELEGATE_OneParam(FOnCreateSessionSuccessDelegate, FCMSSession, Session);
+DECLARE_DYNAMIC_DELEGATE_OneParam(FOnFailureDelegate, FString, ErrorMessage);
+
+UCLASS(BlueprintType, Config=Engine)
+class PREMIERECMSMANAGEMENT_API UPremiereCMSManagementSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+
+	UPremiereCMSManagementSubsystem();
+
+	UPROPERTY()
+	UGraphQLDataSource* GraphQlDataSource;
+	
+	UPROPERTY()
+	USessionRepository* SessionRepository;
+	
+	
+public:
+	UPROPERTY(BlueprintReadOnly, Config, Category="PremiereCMSManagement | Settings")
+	FString GraphQLUrl;
+	
+	UFUNCTION(BlueprintCallable, Category="PremiereCMSManagement")
+	void CreateSession(
+		const FString& Title,
+		const FString& OwnerId,
+		const FString& PerformanceId,
+		FOnCreateSessionSuccessDelegate OnCreateSessionSuccess,
+		FOnFailureDelegate OnCreateSessionFailure
+	);
+};
