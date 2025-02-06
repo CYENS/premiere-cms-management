5613dc7a2146380a1acec199211ae7f410508d03
Chrysostomos Chadjiminas <chadjiminas.chrysostomos@gmail.com>
2025-02-07 00:04:30 +0200

    add constants

---
+++ Source/PremiereCMSManagement/Private/Repositories/GraphQLConstants.cpp
@@ -0,0 +1,57 @@
+﻿
+#include "Repositories/GraphQLConstants.h"
+
+const FString GQLUserFragment = TEXT(R"(
+		fragment userFragment on User {
+		  id
+		  name
+		  email
+		  eosId
+		  userRole
+		  isAdmin
+		  isSuperAdmin
+		  createdAt
+		  person {
+			id
+			givenName
+			familyName
+			artisticName
+		  } 
+		}
+	)");
+
+const FString GQLPerformanceData = TEXT(R"(
+		id
+		title
+		about
+		owner {
+		  ...userFragment
+		}
+		members {
+		  ...userFragment
+		}
+		usdScenes {
+		  id
+		  pCloudFileId
+		  fileUrl
+		  title
+		  owner {
+			...userFragment
+		  }
+		  template
+		  public
+		}
+		sessions {
+		  id
+		  eosSessionId
+		  title
+		  state
+		  owner {
+			...userFragment
+		  }
+		}
+		avatars {
+		  id
+		  name
+		}
+	)");
---
+++ Source/PremiereCMSManagement/Public/Repositories/GraphQLConstants.h
@@ -0,0 +1,8 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+extern const FString GQLUserFragment;
+extern const FString GQLPerformanceData;
