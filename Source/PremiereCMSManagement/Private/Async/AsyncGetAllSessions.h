40e483ef7a6b377d27dad36a41d29f0bb031413e
Chrysostomos Chadjiminas <chadjiminas.chrysostomos@gmail.com>
2025-02-24 11:14:11 +0200

    added AsyncGetAllSessions node

---
+++ Source/PremiereCMSManagement/Private/Async/AsyncGetAllSessions.h
@@ -0,0 +1,37 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintAsyncActionBase.h"
+#include "PremiereCMSManagementSubsystem.h"
+#include "AsyncGetAllSessions.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnCompleteRequest, const TArray<FCMSSession>&, Sessions, FString, ErrorMessage);
+
+UCLASS()
+class PREMIERECMSMANAGEMENT_API UAsyncGetAllSessions : public UBlueprintAsyncActionBase
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, Category = "PremiereCMSManagement | Async | Session", meta = (WorldContext = "WorldContextObject", BlueprintInternalUseOnly =  "true"))
+	static UAsyncGetAllSessions* AsyncGetAllSessions(UObject* WorldContextObject);
+	
+	UPROPERTY(BlueprintAssignable)
+	FOnCompleteRequest OnSuccess;
+
+	UPROPERTY(BlueprintAssignable)
+	FOnCompleteRequest OnFailure;
+	
+	virtual void Activate() override;
+
+protected:
+	UPROPERTY();
+	UObject* WorldContextObject;
+
+
+	UFUNCTION()
+	void HandleSuccess(const TArray<FCMSSession>& Sessions);
+	
+	UFUNCTION()
+	void HandleFailure(const FString& ErrorMessage);
+};
---
+++ Source/PremiereCMSManagement/Public/Async/AsyncGetAllSessions.cpp
@@ -0,0 +1,51 @@
+﻿#include "Async/AsyncGetAllSessions.h"
+#include "Engine/World.h"
+#include "GameFramework/Actor.h"
+
+UAsyncGetAllSessions* UAsyncGetAllSessions::AsyncGetAllSessions(UObject* WorldContextObject)
+{
+	UAsyncGetAllSessions* AsyncNode = NewObject<UAsyncGetAllSessions>();
+	AsyncNode->WorldContextObject = WorldContextObject;
+	return AsyncNode;
+}
+
+void UAsyncGetAllSessions::Activate()
+{
+	if (!WorldContextObject)
+	{
+		HandleFailure(TEXT("Invalid World Context Object"));
+		return;
+	}
+
+	UPremiereCMSManagementSubsystem* CMSSubsystem = WorldContextObject->GetWorld()->GetGameInstance()->GetSubsystem<UPremiereCMSManagementSubsystem>();
+	if (!CMSSubsystem)
+	{
+		HandleFailure(TEXT("Failed to get CMS Subsystem"));
+		return;
+	}
+
+	FOnGetSessions OnGetSession;
+	OnGetSession.BindDynamic(this, &ThisClass::HandleSuccess);
+	
+	FOnFailureDelegate OnFailureDelegate;
+	OnFailureDelegate.BindDynamic(this, &ThisClass::HandleFailure);
+	
+	CMSSubsystem->GetAllSessions(
+		OnGetSession,
+		OnFailureDelegate
+	);
+}
+
+void UAsyncGetAllSessions::HandleSuccess(const TArray<FCMSSession>& Sessions)
+{
+	const FString EmptyErrorMessage = TEXT("");
+	OnSuccess.Broadcast(Sessions, EmptyErrorMessage);
+	SetReadyToDestroy(); 
+}
+
+void UAsyncGetAllSessions::HandleFailure(const FString& ErrorMessage)
+{
+	const TArray<FCMSSession> EmptySessionsArray;
+	OnFailure.Broadcast(EmptySessionsArray, ErrorMessage);
+	SetReadyToDestroy();
+}
